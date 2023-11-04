bd6e61edc3107c4317d6d2f6517e4b81ef7b7210
Upul <upulkr4@gmail.com>
2023-11-04 16:33:28 +0530

    produvt

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import bookingRouter from "./routes/booking.route.js";
+import path from "path";
 // import dotenv from "dotenv";
 
 
 // dotenv.config();
-const mongo='mongodb+srv://saloontoronto:saloontoronto@booking.makscrx.mongodb.net/?retryWrites=true&w=majority'
+const mongo = 'mongodb+srv://saloontoronto:saloontoronto@booking.makscrx.mongodb.net/?retryWrites=true&w=majority'
 mongoose
   .connect(mongo)
   .then(() => {
@@ -15,7 +16,17 @@ mongoose
     console.log(err);
   });
 
+
+const __dirname = path.resolve()
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'client/dist')))
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 app.use(express.json());
 
 app.listen(3000, () => {
@@ -26,7 +37,7 @@ app.use("/api/auth", bookingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message=err.message||"Internal Server error"
+  const message = err.message || "Internal Server error"
   return res.status(statusCode).json({
     success: false,
     statusCode,
