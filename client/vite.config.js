61d4efdff7bbf735c66cfc4e708a529b22dda633
Upul <upulkr4@gmail.com>
2023-11-05 12:26:31 +0530

    gy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-server:{
-  
-  proxy:{
-    '/api':{
-      target:'https://salontest-y3ln.vercel.app:3000',
-      secure:false
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
     },
   },
-},
 
   plugins: [react(),],
 })
+
