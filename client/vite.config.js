1c45161dde9f0d718ec9da9207969cb40bf644f5
Upul  Rathnayaka <93370018+Upulkr@users.noreply.github.com>
2023-11-05 20:08:20 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 
 export default defineConfig({
-
+proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        secure: false,
+      },
+    },
   plugins: [react(),],
 })
 
